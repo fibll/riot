13c235d9f31e1a96e1cac49b8a5ee834e25e87c1
fibll <acc.simonretsch@icloud.com>
2017-10-23 12:44:56 +0200

    ADD hello_world programm copied from riot project ADD note to anmerkungen.txt

---
+++ hello-world/main.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Hello World application
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\n\n\n\n");
+    puts("Hello out there!");
+
+    printf("Board: %s\n", RIOT_BOARD);
+    printf("Featured MCU: %s\n", RIOT_MCU);
+
+    return 0;
+}
